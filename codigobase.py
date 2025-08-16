79c4c9a924a9a9e37454135520023efd5b66cc7f
souviitor <viitorsouza93@gmail.com>
2025-08-16 16:20:10 -0300

    alteração

--- codigobase.py
+++ codigobase.py
@@ -9,7 +9,9 @@ import streamlit as st
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
 #MELHORANDO O NOME DAS COLUNAS DA TABELA
-df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos',
+            'deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes',
+            'totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
 
 #SELECÃO DO ESTADO
 estados = list(df['state'].unique())
@@ -17,7 +19,8 @@ state = st.sidebar.selectbox('Qual estado?', estados)
 
 #SELEÇÃO DA COLUNA
 column ='Casos por 100 mil habitantes'
-colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes',
+            'Casos por 100 mil habitantes']
 column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
 #SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO
